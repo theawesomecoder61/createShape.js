a2e60782852eacb146837bffcec4e9dc21bbd61f
Andrew <hellothere61@users.noreply.github.com>
2014-09-04 14:02:58 -0400

    Update createShape.js

--- createShape.js
+++ createShape.js
@@ -1,1 +1,2 @@
+// don't know why i had to do this, but oh well
 window.onload=function(){var e=document.createElement("script");e.src="http://theawesomecoder61.github.io/createShape.js/js/createShape.js";document.getElementsByTagName("head")[0].appendChild(e)}
