99d62f927abca2ce4ae14505424c85359c8048bd
Andrew <hellothere61@users.noreply.github.com>
2014-09-04 12:17:18 -0400

    Update createShape.js

--- createShape.js
+++ createShape.js
@@ -17,80 +17,91 @@ need:
 */
 
 function createShape(options) {
-// creates a new canvas if it doesn't exist
-if(!document.querySelector(options.canvasQuery)) {
+  // creates a new canvas if it doesn't exist
+  if(!document.querySelector(options.canvasQuery)) {
 
-  var newCanvas = document.createElement("canvas");
+    var newCanvas = document.createElement("canvas");
 
-  if(options.canvasQuery.indexOf(".") > -1) {
-    newCanvas.setAttribute("class", options.canvasQuery.replace(".", ""));
+    if(options.canvasQuery.indexOf(".") > -1) {
+      newCanvas.setAttribute("class", options.canvasQuery.replace(".", ""));
+    }
+    if(options.canvasQuery.indexOf("#") > -1) {
+      newCanvas.id = options.canvasQuery.replace("#", "");
+    }
+
+    newCanvas.setAttribute("width", options.canvasWidth);
+    newCanvas.setAttribute("height", options.canvasHeight);
+
+    document.querySelector(options.appendTo).appendChild(newCanvas);
   }
-  if(options.canvasQuery.indexOf("#") > -1) {
-    newCanvas.id = options.canvasQuery.replace("#", "");
+
+  // variables
+  var canvas = document.querySelector(options.canvasQuery);
+  var context = canvas.getContext("2d");
+
+  // begins path
+  context.beginPath();
+
+  // rectangle
+  if(options.shape == "rectangle" || options.shape == "rect") {
+    context.rect(options.x, options.y, options.width, options.height);
   }
 
-  newCanvas.setAttribute("width", options.canvasWidth);
-  newCanvas.setAttribute("height", options.canvasHeight);
+  // circle
+  if(options.shape == "circle") {
+    context.arc(options.x, options.y, options.radius, 0, 2 * Math.PI, false);
+  }
 
-  document.querySelector(options.appendTo).appendChild(newCanvas);
-}
+  // semicircle
+  if(options.shape == "semicircle") {
+    context.arc(options.x, options.y, options.radius, 0, 1 * Math.PI, false);
+  }
 
-// variables
-var canvas = document.querySelector(options.canvasQuery);
-var context = canvas.getContext("2d");
+  // text
+  if(options.shape == "text") {
+    context.font = options.font;
+    context.fillText(options.text, options.x, options.y);
+    context.textAlign = options.textAlign;
+  }
 
-// begins path
-context.beginPath();
+  // image
+  if(options.shape == "image") {
+    var newImg = new Image();
+    newImg.onload = function() {
+      context.drawImage(newImg, options.x, options.y, options.width, options.height);
+    };
+    newImg.src = options.src;
+  }
 
-// line
-if(options.shape == "line") {
-  context.moveTo(options.x1, options.y1);
-  context.lineTo(options.x2, options.y2);
+  // alpha
+  if(options.opacity !== "" || options.opacity !== null) {
+    context.globalAlpha = options.opacity;
+  }
+
+  // translate
+  if(options.translate !== "" || options.translate !== null) {
+    // context.translate(options.translate); screws up shapes
+  }
+
+  // scale
+  if(options.scale !== "" || options.scale !== null) {
+    // context.scale(options.scale); screws up shapes as well
+  }
+
+  // flip
+  if(options.flip == "h" || options.flip == "horizontal" || options.flip == "horizontally") {
+    context.scale(-1, 1);
+  }
+  if(options.flip == "v" || options.flip == "vertical" || options.flip == "vertically") {
+    context.scale(1, -1);
+  }
+
+  // fill
+  context.fillStyle = options.fillColor;
+  context.fill();
+
+  // stroke
+  context.lineWidth = options.strokeWidth;
+  context.strokeStyle = options.strokeColor;
   context.stroke();
-}
-
-// rectangle
-if(options.shape == "rectangle" || options.shape == "rect") {
-  context.rect(options.x, options.y, options.width, options.height);
-}
-
-// circle
-if(options.shape == "circle") {
-  context.arc(options.x, options.y, options.radius, 0, 2 * Math.PI, false);
-}
-
-// semicircle
-if(options.shape == "semicircle") {
-  context.arc(options.x, options.y, options.radius, 0, 1 * Math.PI, false);
-}
-
-// text
-if(options.shape == "text") {
-  context.font = options.font;
-  context.fillText(options.text, options.x, options.y);
-}
-
-// image
-if(options.shape == "image") {
-  var newImg = new Image();
-  newImg.onload = function() {
-    context.drawImage(newImg, options.x, options.y, options.width, options.height);
-  };
-  newImg.src = options.src;
-}
-
-// fill
-context.fillStyle = options.fillColor;
-
-// stroke
-context.lineWidth = options.strokeWidth;
-context.strokeStyle = options.strokeColor;
-
-// line cap
-context.lineCap = options.lineCap;
-
-// last lines!
-context.fill();
-context.stroke();
-
 }
